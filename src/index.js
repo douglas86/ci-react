17c52da0940a9c5b1c7ab22dd280448d74482c66
douglas <douglasmaxton@gmail.com>
2024-04-03 14:46:38 +0100

    changed: file extensions to .jsx

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './static/css/index.css';
+import App from './constructors/App.jsx';
+import reportWebVitals from './constructors/reportWebVitals.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
